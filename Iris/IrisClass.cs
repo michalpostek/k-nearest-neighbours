2da5f9469385d7d0ec7bfb40dbc6be242bcc4bc5
Michał Postek <postek.michal@protonmail.com>
2025-05-20 17:58:51 +0200

    create Iris

---
+++ Iris/IrisClass.cs
@@ -0,0 +1,8 @@
+﻿namespace Iris;
+
+public enum IrisClass
+{
+    First = 1,
+    Second = 2,
+    Third = 3
+}
---
+++ Iris/IrisDataReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Iris;
+
+public static class IrisDataReader
+{
+    public static List<Tuple<double[], IrisClass>> ReadData()
+    {
+        var path = Path.Combine(AppContext.BaseDirectory, "..","..","..", "sample_training_data");
+        var lines = File.ReadAllLines(path);
+        
+        return lines.Select(ParseSample).ToList();
+    }
+    
+    private static Tuple<double[], IrisClass> ParseSample(string line)
+    {
+        var properties = line
+            .Split()
+            .Select(x => double.Parse(x, CultureInfo.InvariantCulture))
+            .ToArray();
+
+        if (properties.Length != 5)
+        {
+            throw new ArgumentException("Invalid sample line");
+        }
+
+        return new Tuple<double[], IrisClass>(properties.Take(4).ToArray(), (IrisClass)properties.Last());
+    }
+}
